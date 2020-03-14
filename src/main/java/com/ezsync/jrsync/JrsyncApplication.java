afed91562d49c9ae1c03ef0b0b8adf7d59c4c4ba
chenwj <chenwj@chenwjdeMacBook-Pro.local>
2020-03-14 20:24:58 +0800

    Initial commit

---
+++ src/main/java/com/ezsync/jrsync/JrsyncApplication.java
@@ -0,0 +1,13 @@
+package com.ezsync.jrsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JrsyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ezsync/jrsync/JrsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezsync.jrsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrsyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
